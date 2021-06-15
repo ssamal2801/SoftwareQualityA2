73efd4b72dbef9b737b509657687549067d610c1
swagathub <sinu2290@gmail.com>
2021-06-14 21:01:05 -0400

    Test Projected Created

---
+++ SoftwareQualityA2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SoftwareQualityA2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SoftwareQualityA2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3cad9201-85b2-45f0-a37f-adc82e150fa8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SoftwareQualityA2.Test/RectangleTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SoftwareQualityA2.Test
+{
+    [TestClass]
+    public class RectangleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
